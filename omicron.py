4820d2e51a1b0cee9d9de2881bc38829811fabe1
Francisco Javier Hernández Montoya <fhm850@inlumine.ual.es>
2021-03-17 19:59:09 +0100

    Prueba

--- omicron.py
+++ omicron.py
@@ -1,25 +1,26 @@
 MAX = 100
 
+class tipo_v_p(): 
+    vector = [1 for i in range(MAX)]
+
 class tipo_poligono():
     n: int
     v: tipo_v_p
 
-class tipo_v_p(): 
-    vector[1..MAX] type tipo_punto2d
-
 class tipo_punto2d():
-        x,y: float
+        x: float
+        y: float
+
+class tipo_v_t():
+     vector = [1 for i in range(MAX)]
 
 class tipo_triangulacion:
         n: int
         t: tipo_v_t
 
-class tipo_v_t():
-     vector[1..MAX] de tipo_tri
-
 class tipo_tri():
-     vector[1..3] de tipo_punto2d
+     vector = [1 for i in range(3)]
 
-def triangulacion(tipo_poligono poligono, tipo_v_p vectores):
+def triangulacion(tipo_poligono, tipo_v_p ):
 
     return 
