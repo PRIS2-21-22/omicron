ee50848809cc3b83f14d8e0db648491d09893c88
Francisco Javier Hernández Montoya <fhm850@inlumine.ual.es>
2021-03-24 20:04:22 +0100

    Duplicado

--- centroide.py
+++ centroide.py
@@ -17,24 +17,20 @@ class Centroide():
                 suma = suma + (self.poligono.puntos[i].get_x()*self.poligono.puntos[0].get_y() - self.poligono.puntos[0].get_x()*self.poligono.puntos[i].get_y())
         return suma/2
 
-    #Método que nos calcula la abscisa(x) del centroide
-    def xcent(self,area):
+    #Método que nos calcula la abscisa(x) y la ordenada(y) del centroide
+    def cent(self,area,xy):
         suma = 0
         for i in range(self.poligono.lados):
             if(i != self.poligono.lados-1):
-                suma = suma + (self.poligono.puntos[i+1].get_x() + self.poligono.puntos[i].get_x())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[i+1].get_y() - self.poligono.puntos[i+1].get_x()*self.poligono.puntos[i].get_y())
+                if(xy == "x"):
+                    suma = suma + (self.poligono.puntos[i+1].get_x() + self.poligono.puntos[i].get_x())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[i+1].get_y() - self.poligono.puntos[i+1].get_x()*self.poligono.puntos[i].get_y())
+                else:
+                    suma = suma + (self.poligono.puntos[i+1].get_y() + self.poligono.puntos[i].get_y())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[i+1].get_y() - self.poligono.puntos[i+1].get_x()*self.poligono.puntos[i].get_y())
             else:
-                suma = suma + (self.poligono.puntos[0].get_x() + self.poligono.puntos[i].get_x())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[0].get_y() - self.poligono.puntos[0].get_x()*self.poligono.puntos[i].get_y())
-        return suma/(6*area)
-
-    #Método que nos calcula la ordenada(y) del centroide
-    def ycent(self,area):
-        suma = 0
-        for i in range(self.poligono.lados):
-            if(i != self.poligono.lados-1):
-                suma = suma + (self.poligono.puntos[i+1].get_y() + self.poligono.puntos[i].get_y())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[i+1].get_y() - self.poligono.puntos[i+1].get_x()*self.poligono.puntos[i].get_y())
-            else:
-                suma = suma + (self.poligono.puntos[0].get_y() + self.poligono.puntos[i].get_y())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[0].get_y() - self.poligono.puntos[0].get_x()*self.poligono.puntos[i].get_y())
+                if(xy == "x"):
+                    suma = suma + (self.poligono.puntos[0].get_x() + self.poligono.puntos[i].get_x())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[0].get_y() - self.poligono.puntos[0].get_x()*self.poligono.puntos[i].get_y())
+                else:
+                    suma = suma + (self.poligono.puntos[0].get_y() + self.poligono.puntos[i].get_y())*(self.poligono.puntos[i].get_x()*self.poligono.puntos[0].get_y() - self.poligono.puntos[0].get_x()*self.poligono.puntos[i].get_y())
         return suma/(6*area)
 
 class main():
@@ -58,8 +54,8 @@ class main():
 
     centroide = Centroide(poligono = poligono)
     area = centroide.area()
-    xcent = centroide.xcent(area = area)
-    ycent = centroide.ycent(area = area)
+    xcent = centroide.cent(area = area, xy = "x")
+    ycent = centroide.cent(area = area, xy = "y")
 
     punto_centroide = punto.Punto(x = xcent, y = ycent)
     print(f"El centroide del poligono introducido es: {punto_centroide.to_string()}")
